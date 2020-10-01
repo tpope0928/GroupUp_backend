753f5d7642a86496c721877d3c398c0612dcd9b9
tpope0928 <tpope0928@gmail.com>
2020-09-30 20:54:15 -0400

    player controller logic

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class Api::V1::PlayersController < ApplicationController
+  wrap_parameters :player, include: %i[name city state game_ids]
+
+  def index
+    players = Player.all
+    render json: PlayerSerializer.new(players)
+  end
+
+  def create
+    player = Player.new(player_params)
+    if player.save
+      render json: PlayerSerializer.new(player), status: :accpeted
+    else
+      render json:  { errors: player.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :city, :state, :game_ids => [])
+  end
 end
