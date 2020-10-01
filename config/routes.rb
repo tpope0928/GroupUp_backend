b1cf907942295312798c60f35044e15d8336a358
tpope0928 <tpope0928@gmail.com>
2020-09-30 21:01:42 -0400

    routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+
 Rails.application.routes.draw do
-  resources :usernames
-  resources :games
-  resources :players
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/test', to: 'application#test'
+  namespace :api do
+    namespace :v1 do
+      resources :players, only: [:index, :create]
+      resources :games, only: [:index, :create]
+    end
+  end
 end
