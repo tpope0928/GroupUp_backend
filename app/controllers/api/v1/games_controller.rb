b6b7ed6cc8d81c0b89bcdee1eae7fd3b6100fdbd
tpope0928 <tpope0928@gmail.com>
2020-09-30 21:12:16 -0400

    games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::GamesController < ApplicationController
+  wrap_parameters :game, include: %i[title genre skill_level players]
 
+  def index
+    games = Game.all
+    render json: GameSerializer.new(games)
+  end
+
+  def create
+    game = Game.new(game_params)
+
+    if game.save
+      render json: GameSerializer.new(game), status: :accepted
+    else
+      render json: { errors: game.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :skill_level, :player_ids)
+  end
 end
