b320941b20b7123ed129f325f387547171ef6a95
tpope0928 <tpope0928@gmail.com>
2020-12-20 20:31:57 -0500

    validation work

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,9 @@
 class Player < ApplicationRecord
+
   has_many :usernames
   has_many :games, through: :usernames
 
   accepts_nested_attributes_for :games
+
+  validates :name, uniqueness: { case_sensitive: false }
 end
